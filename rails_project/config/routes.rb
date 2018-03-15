0452afcd0840927b2500f8af76d4accaf0ed128b
Robert Harrison <>
2018-03-15 16:43:53 -0400

    user signup

--- rails_project/config/routes.rb
+++ rails_project/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
+  get  '/help',    to: 'static_pages#help'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
